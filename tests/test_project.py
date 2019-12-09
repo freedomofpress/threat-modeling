d10dddf00cf9544abe5bedcbad0d2677acdba7be
redshiftzero <jen@freedom.press>
2019-12-08 21:53:15 -0500

    test: adding threats to threat model object
    I think it makes sense to require an identifier (either str or UUID -
    str such that if the threat modeler has some predefined IDs we can
    pass them in, else a UUID will get generated) for __getitem__
    and __contains__. If they want to use the Element and Threat objects
    themselves to check for membership in the ThreatModel the developer
    should access the lists directly, i.e. threat in ThreatModel.threats.

--- tests/test_project.py
+++ tests/test_project.py
@@ -3,6 +3,7 @@ import pytest
 from threat_modeling.data_flow import Element
 from threat_modeling.exceptions import DuplicateIdentifier
 from threat_modeling.project import ThreatModel
+from threat_modeling.threats import Threat
 
 
 def test_threat_model_saves_elements():
@@ -11,7 +12,7 @@ def test_threat_model_saves_elements():
 
     my_threat_model.add_element(server)
 
-    assert server in my_threat_model
+    assert server.identifier in my_threat_model
 
 
 def test_threat_model_get_element_by_id():
@@ -30,6 +31,54 @@ def test_threat_model_disallows_adding_duplicate_elements():
     my_threat_model = ThreatModel()
 
     my_threat_model.add_element(server)
+
+    with pytest.raises(DuplicateIdentifier):
+        my_threat_model.add_element(server)
+
+
+def test_threat_model_saves_threats():
+    """This is used by an enumeration method or if the user wants to
+    manually define threats"""
+
+    tamper_traffic = Threat(description="Attacker tampers with user's network traffic")
+
+    # Ensures that an identifying uuid has been generated if it wasn't provided
+    assert tamper_traffic.identifier
+
+    my_threat_model = ThreatModel()
+
+    my_threat_model.add_threat(tamper_traffic)
+
+    assert tamper_traffic.identifier in my_threat_model
+
+
+def test_threat_model_get_threat_by_id():
+    tamper_traffic = Threat(description="Attacker tampers with user's network traffic")
+    my_threat_model = ThreatModel()
+
+    my_threat_model.add_threat(tamper_traffic)
+
+    assert my_threat_model[tamper_traffic.identifier]
+
+
+def test_threat_model_disallows_adding_duplicate_threats():
+    tamper_traffic = Threat(description="Attacker tampers with user's network traffic")
+    my_threat_model = ThreatModel()
+
+    my_threat_model.add_threat(tamper_traffic)
+
+    with pytest.raises(DuplicateIdentifier):
+        my_threat_model.add_threat(tamper_traffic)
+
+
+def test_threat_model_disallows_adding_threats_that_duplicate_an_element():
+    tamper_traffic = Threat(description="This doesn't really matter")
+    test_id = "Server"
+    server = Element(identifier=test_id)
+    server.identifier = tamper_traffic.identifier
+    my_threat_model = ThreatModel()
+
+    my_threat_model.add_threat(tamper_traffic)
 
     with pytest.raises(DuplicateIdentifier):
         my_threat_model.add_element(server)
