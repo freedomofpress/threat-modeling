bfb885d806c015a609d5f039728d684de40607b0
redshiftzero <jen@freedom.press>
2020-01-23 23:32:38 -0500

    packaging: add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="threat_modeling",
+    version="0.0.1",
+    author="redshiftzero",
+    author_email="jen@redshiftzero.com",
+    description="Threat modeling tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/redshiftzero/threat_modeling",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Security",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
