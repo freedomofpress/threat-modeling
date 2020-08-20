8221e2d83b50fb3f1fa0685a085e5096b49ec9b7
redshiftzero <jen@freedom.press>
2020-08-20 18:03:34 -0400

    readthedocs integration

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
